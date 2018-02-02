cb538b4f69f5548c92abd22aaceb4c1b3736aa43
Abner-Qin <1850208275@qq.com>
2018-02-02 15:12:22 +0800

    仿快餐pos机点餐系统

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="left-nav">
+<ul>
+<li>
+<i class="icon iconfont icon-huigou009"></i>
+<div>收银</div>
+<i class="icon iconfont icon-dianpu"></i>
+<div>店铺</div>
+<i class="icon iconfont icon-hanbao"></i>
+<div>商品</div>
+<i class="icon iconfont icon-qiapian"></i>
+<div>会员</div>
+<i class="icon iconfont icon-classify_zuanleishipin"></i>
+<div>统计</div>
+<i class="icon iconfont icon-shezhi"></i>
+<div>设置</div>
+</li>
+</ul>
+</div>
+</template>	
+<script>
+export default{
+	
+}
+</script>
+<style>
+.left-nav{color:#fff;
+font-size:10px;
+height:100%;
+background-color:#1d8ce0;
+float:left;
+width:5%;
+}
+.iconfont{
+	font-size:24px;
+}
+.left-nav ul{
+	padding:0;margin:0;
+}
+.left-nav ul li{
+	list-style:none;
+	text-align:center;
+	border-bottom:1px solid #20a0ff;
+	padding:10px;
+}
+</style>		
